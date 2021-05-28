101173eda82d17ef12908ef5a72046b8b0e76ad5
Grzegorz Kocoł <grzesiek.k511@wp.pl>
2021-05-28 09:04:59 +0200

    improved naming

--- Program.cs
+++ Program.cs
@@ -3,26 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices.ComTypes;
-
-
-var bytes = 0;
-Console.WriteLine($"{Convert.ToString(bytes, 2)}");
-
 
 
 var currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 var path = Path.Combine(currentPath + @"\dane9.txt"); // Zmień nazwe pliku, jeśli chcesz inny wynik
-var listOfNumbers = new List<Point>();
-string line;
-
+var tasks = new List<Task>();
 using (StreamReader sr = new(path))
 {
-    line = sr.ReadLine();
+    var line = sr.ReadLine();
     while ((line = sr.ReadLine()) is not null)
     {
         var splitLine = line.Split(' ');
-        listOfNumbers.Add(new Point
+        tasks.Add(new Task
         {
             CzasWykononania = int.Parse(splitLine[0]),
             WspolczynnikKary = int.Parse(splitLine[1]),
@@ -32,10 +24,10 @@ using (StreamReader sr = new(path))
     }
 }
 
-Console.WriteLine($"Wynik:{BandB(listOfNumbers)} ");
+Console.WriteLine($"Wynik:{BandB(tasks)} ");
 
 
-static int BandB(List<Point> tasks)
+static int BandB(List<Task> tasks)
 {
     List<int> opts = new();
     for (int i = 0; i <= Math.Pow(2, tasks.Count) - 1; i++)
@@ -45,19 +37,17 @@ static int BandB(List<Point> tasks)
     return opts.Last();
 }
 
-static int CountOpt(List<Point> tasks, string bitString, List<int> opt)
+static int CountOpt(List<Task> tasks, string bitString, List<int> opt)
 {
-    int min = int.MaxValue, optCounted, elementAt;
+    int min = int.MaxValue;
     if (bitString.Length == 1 && bitString[0] == '0') return 0;
     for (int i = bitString.Length - 1; i >= 0; i--)
     {
         if (IfBitChecked(bitString, i))// Ostatni bit zawsze jest 1 elementem w taskach
         {
-            elementAt = Convert.ToInt32(bitString.ReplaceAt(i, '0'), 2);
-
-            var sum = SumTaskWorkTime(bitString, tasks) - tasks[bitString.Length - 1 - i].ZadanyTerminEnd;
-            optCounted = opt[elementAt] + tasks[bitString.Length -1 - i].WspolczynnikKary
-                * Math.Max(0, sum);
+            var elementAt = Convert.ToInt32(bitString.ReplaceAt(i, '0'), 2);
+            var optCounted = opt[elementAt] + tasks[bitString.Length -1 - i].WspolczynnikKary
+                * Math.Max(0, SumTaskWorkTime(bitString, tasks) - tasks[bitString.Length - 1 - i].ZadanyTerminEnd);
             if (min > optCounted)
                 min = optCounted;
         }
@@ -71,12 +61,12 @@ static bool IfBitChecked(string bitString, int index)
     return bitString[index] == '1';
 }
 
-static int SumTaskWorkTime(string bitString, IEnumerable<Point> tasks)
+static int SumTaskWorkTime(string bitString, IEnumerable<Task> tasks)
 {
     int sum = 0;
     for (int i = 0; i < bitString.Length; i++)
     {
-        if (IfBitChecked(bitString, bitString.Length -1 - i))
+        if (IfBitChecked(bitString, bitString.Length -1 - i))//ostatni bit => pierwszym zadaniem
             sum += tasks.ElementAt(i).CzasWykononania;
     }
 
@@ -97,7 +87,7 @@ public static class StringExtensions
     }
 }
 
-class Point
+class Task
 {
     //J - zadanie
     public int CzasWykononania { get; set; } // pj
